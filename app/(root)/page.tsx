6676ead43d03eede15cb1b17e730c3da9a6f6e9e
Ako2003 <akifmursalov@gmail.com>
2023-08-28 12:49:37 +0400

    Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,9 @@ import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
-    <div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
       <UserButton afterSignOutUrl="/sign-up"/>
-    </div>
+    </>
   )
 }
