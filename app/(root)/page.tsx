e0c70acdba41567495a73cb7b8a92cadd9f4db3c
Ako2003 <akifmursalov@gmail.com>
2023-08-29 00:40:23 +0400

    New features

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      <UserButton afterSignOutUrl="/sign-up"/>
+      <UserButton afterSignOutUrl="/sign-in"/>
     </>
   )
 }
