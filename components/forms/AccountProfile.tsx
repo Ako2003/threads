bda42343d3da227450b5dd88e11dd3b8bed05391
Ako2003 <akifmursalov@gmail.com>
2023-08-29 02:00:05 +0400

    NEW!

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { useForm } from 'react-hook-form';
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { isBase64Image } from '@/lib/utils';
 import {
   Form,
   FormControl,
@@ -12,11 +13,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from '@/lib/validations/user';
 import * as z from 'zod';
 import Image from 'next/image';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { useUploadThing } from '@/lib/uploadthing';
 
 interface Props {
     user: {
@@ -31,24 +34,54 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+    const [files, setFiles] = useState<File[]>([])
+    const { startUpload } = useUploadThing("media");
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: '',
-            name: '',
-            username: '',
-            bio: ''
+            profile_photo: user?.image || "",
+            name: user?.name || '',
+            username: user?.username || '',
+            bio: user?.bio || ''
         }
     });
 
-    const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
         e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if(e.target.files && e.target.files.length > 0){
+          const file = e.target.files[0];
+
+          setFiles(Array.from(e.target.files));
+
+          if(!file.type.includes('image')) return;
+
+          fileReader.onload = async (event) => {
+            const imageDataUrl = event.target?.result?.toString() || '' ;
+
+            fieldChange(imageDataUrl);
+          }
+
+          fileReader.readAsDataURL(file);
+        }
+
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof UserValidation>) {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if(hasImageChanged){
+          const imgRes = await startUpload(files)
+
+          if(imgRes && imgRes[0].fileUrl){
+            values.profile_photo = imgRes[0].fileUrl;
+          }
+        }
       }
 
     return (
@@ -90,14 +123,65 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                   />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+
+            <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className='flex flex-col gap-3 w-full'>
+                <FormLabel className=' text-base-semibold text-light-2'>
+                    Name
+                </FormLabel>
+                <FormControl>
+                  <Input 
+                  type='text'
+                  className='account-form_input'
+                  {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+            <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className='flex flex-col gap-3 w-full'>
+                <FormLabel className='text-base-semibold text-light-2'>
+                    Username
+                </FormLabel>
+                <FormControl>
+                  <Input 
+                  type='text'
+                  className='account-form_input'
+                  {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+            <FormField
+            control={form.control}
+            name="bio"
+            render={({ field }) => (
+              <FormItem className='flex flex-col gap-3 w-full'>
+                <FormLabel className='text-base-semibold text-light-2'>
+                    Bio
+                </FormLabel>
+                <FormControl className='flex-1 text-base-semibold text-gray-200'>
+                  <Textarea 
+                    rows={10}
+                    className='account-form_input'
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className='bg-primary-500'>Submit</Button>
         </form>
       </Form>
     )
